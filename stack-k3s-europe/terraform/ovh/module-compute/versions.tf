e27529a3969ca7ac54a998de9bbdfa3a06ff40f4
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-18 18:28:27 +0100

    minor fix

--- stack-k3s-europe/terraform/ovh/module-compute/versions.tf
+++ stack-k3s-europe/terraform/ovh/module-compute/versions.tf
@@ -1,11 +1,5 @@
 terraform {
   required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-    }
-    ovh = {
-      source  = "ovh/ovh"
-    }
     cycloid = {
       source = "cycloidio/cycloid"
     }
