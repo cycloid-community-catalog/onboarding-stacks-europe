12dd2b22f5ad1df817b6ea1d7e452dc74faf1dc9
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-19 14:41:45 +0100

    minor fix

--- stack-k3s-europe/terraform/ovh/provider.tf
+++ stack-k3s-europe/terraform/ovh/provider.tf
@@ -1,6 +1,7 @@
 provider "openstack" {
   auth_url    = "https://auth.cloud.ovh.net/v3/" # Authentication URL
   domain_name = "default" # Domain name - Always at 'default' for OVHcloud
+  region      = "BHS5"
   tenant_name = var.ovh_cred.tenant_id
   user_name   = var.ovh_cred.username
   password    = var.ovh_cred.password
