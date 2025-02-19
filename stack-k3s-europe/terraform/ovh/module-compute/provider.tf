04ed04f7f7ac6b8a6d30a0cfb8bf2222feca1346
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-19 17:45:49 +0100

    minor fix

--- stack-k3s-europe/terraform/ovh/module-compute/provider.tf
+++ stack-k3s-europe/terraform/ovh/module-compute/provider.tf
@@ -1,4 +1,7 @@
 provider "openstack" {
   auth_url    = "https://auth.cloud.ovh.net/v3/" # Authentication URL
   domain_name = "default" # Domain name - Always at 'default' for OVHcloud
+  region      = var.ovh_region
+  user_name   = var.ovh_cred.username
+  password    = var.ovh_cred.password
 }
--- stack-k3s-europe/terraform/ovh/provider.tf
+++ stack-k3s-europe/terraform/ovh/provider.tf
@@ -1,6 +1,10 @@
 provider "openstack" {
   auth_url    = "https://auth.cloud.ovh.net/v3/" # Authentication URL
   domain_name = "default" # Domain name - Always at 'default' for OVHcloud
+  alias       = "ovh" # An alias
+  region      = var.ovh_region
+  user_name   = var.ovh_cred.username
+  password    = var.ovh_cred.password
 }
 
 provider "cycloid" {
