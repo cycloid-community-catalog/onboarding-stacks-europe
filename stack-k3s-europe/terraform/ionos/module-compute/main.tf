f1f1477ff9c428651ada83c4e712b52eb06d3ccc
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-20 00:35:41 +0100

    minor fix

--- stack-k3s-europe/terraform/ionos/module-compute/main.tf
+++ stack-k3s-europe/terraform/ionos/module-compute/main.tf
@@ -21,16 +21,23 @@ resource "ionoscloud_server" "compute" {
     firewall_active = false
   }
 
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = tls_private_key.ssh_key.private_key_openssh
+    host        = ionoscloud_server.compute.primary_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 60",
+    ]
+  }
+
   provisioner "file" {
     source      = "/etc/rancher/k3s/k3s.yaml"
     destination = "kubeconfig"
 
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = tls_private_key.ssh_key.private_key_openssh
-      host        = ionoscloud_server.compute.primary_ip
-    }
   }
 }
 
