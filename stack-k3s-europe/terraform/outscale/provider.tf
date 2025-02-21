6406e6db84c7985eb093626b07d958bcd52b4359
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-21 17:57:07 +0100

    small fix

--- stack-k3s-europe/terraform/outscale/provider.tf
+++ stack-k3s-europe/terraform/outscale/provider.tf
@@ -1,6 +1,6 @@
 provider "outscale" {
-  access_key_id   = var.outscale_cred.access_id
-  secret_key_id   = var.outscale_cred.secret_id
+  access_key_id   = var.outscale_cred.access_key
+  secret_key_id   = var.outscale_cred.secret_key
   region          = var.outscale_region
 }
 
