0bc744f009abde27ab9a0ef501885f76f8745194
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-24 23:37:17 +0100

    small fix

---
+++ stack-lz-europe/terraform/module-lz/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    outscale = {
+      source = "outscale/outscale"
+    }
+
+    ionoscloud = {
+      source = "ionos-cloud/ionoscloud"
+    }
+
+    cycloid = {
+      source = "cycloidio/cycloid"
+    }
+  }
+}
