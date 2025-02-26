ba41db953ba1028529c34447f374d4443c097c04
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-02-26 10:31:07 +0100

    small fix

--- stack-nexus-k8s-demo/terraform/variables.tf
+++ stack-nexus-k8s-demo/terraform/variables.tf
@@ -3,3 +3,15 @@ variable "cy_org" {}
 variable "cy_pro" {}
 variable "cy_env" {}
 variable "cy_com" {}
+
+# Cycloid
+variable "cycloid_api_url" {
+  type        = string
+  description = "Cycloid API endpoint"
+}
+
+variable "cycloid_jwt" {
+  type        = string
+  description = "Org JWT used for authentication"
+  sensitive   = true
+}
