03cafb0be1ecc91b7db86b9aa5d1382535ada2f8
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-03-12 14:12:06 +0100

    minor change

--- stack-compute-aws-demo/terraform/main.tf
+++ stack-compute-aws-demo/terraform/main.tf
@@ -14,29 +14,29 @@ module "compute" {
 
   #. vm_instance_type: 't3.micro'
   #+ Instance type for the VM
-  vm_instance_type = "Value injected by StackForms"
+  vm_instance_type = ""
 
   #. vm_disk_size: 20
   #+ Disk size for the VM (Go)
-  vm_disk_size = "Value injected by StackForms"
+  vm_disk_size = ""
 
-  #. vm_ports_in: ["80", "443"]
+  #. vm_ports_in: [80, 443]
   #+ Ingress TCP ports allowed from the internet
-  vm_ports_in = ["80", "443"]
+  vm_ports_in = [80, 443]
 
   #. res_selector: ''
   #+ Whether to create a new VPC or select an existing one
-  res_selector = "Value injected by StackForms"
+  res_selector = ""
 
   #. vpc_id_inventory: ''
   #+ VPC where to deploy the resources
-  vpc_id_inventory = "Value injected by StackForms"
+  vpc_id_inventory = ""
 
   #. vpc_id_aws: ''
   #+ VPC where to deploy the resources
-  vpc_id_aws = "Value injected by StackForms"
+  vpc_id_aws = ""
 
   #. vpc_id_manual: ''
   #+ VPC where to deploy the resources
-  vpc_id_manual = "Value injected by StackForms"
+  vpc_id_manual = ""
 }
