1c119bdb55c782233c214fb17cfbd550f4524126
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-04-09 23:05:13 +0200

    minor fix

--- stack-postgresql/terraform/aws/module-rds/parameter_group.tf
+++ stack-postgresql/terraform/aws/module-rds/parameter_group.tf
@@ -1,4 +1,4 @@
 resource "aws_db_parameter_group" "rds-optimized" {
   name        = "${var.cy_org}-${var.cy_project}-${var.cy_env}-${var.cy_component}"
-  family      = "postgres"+substr(var.rds_engine_version, 0, 2)
+  family      = format("postgres%s", substr(var.rds_engine_version, 0, 2))
 }
