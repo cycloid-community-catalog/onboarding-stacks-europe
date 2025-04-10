b0edc1e8ba8b45ae2affc13ee4e87d8b04115dc1
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-04-10 15:04:35 +0200

    minor fix

--- stack-postgresql/terraform/aws/module-rds/network.tf
+++ stack-postgresql/terraform/aws/module-rds/network.tf
@@ -18,10 +18,10 @@ module "vpc" {
 
   name = "${var.cy_org}-${var.cy_project}-${var.cy_env}-${var.cy_component}"
   
-  azs                 = ["${var.aws_region}a"]
-  cidr                = "10.77.0.0/16"
-  private_subnets     = ["10.77.0.0/19"]
-  public_subnets      = ["10.77.96.0/19"]
+  azs                 = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  cidr                = ["10.77.0.0/16", "10.77.0.0/16", "10.77.0.0/16"]
+  private_subnets     = ["10.77.1.0/24", "10.77.2.0/24", "10.77.3.0/24"]
+  public_subnets      = ["10.77.101.0/24", "10.77.102.0/24", "10.77.103.0/24"]
 
   enable_nat_gateway      = false
   enable_dns_hostnames    = true
