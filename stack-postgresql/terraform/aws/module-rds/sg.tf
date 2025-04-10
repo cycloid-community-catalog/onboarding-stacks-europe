418d17592921836124caa425bfc75b93023d7acc
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-04-10 14:54:08 +0200

    minor fix

--- stack-postgresql/terraform/aws/module-rds/sg.tf
+++ stack-postgresql/terraform/aws/module-rds/sg.tf
@@ -8,11 +8,11 @@ resource "aws_security_group" "rds" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "mysql" {
+resource "aws_vpc_security_group_ingress_rule" "postgresql" {
   security_group_id = aws_security_group.rds.id
-  cidr_ipv4         = data.aws_vpc.selected[0].cidr_block
-  from_port         = 3306
-  to_port           = 3306
+  cidr_ipv4         = var.res_selector == "create" ? module.vpc[0].cidr_block : data.aws_vpc.selected[0].cidr_block
+  from_port         = 5432
+  to_port           = 5432
   ip_protocol       = "tcp"
 
   # referenced_security_group_id = aws_security_group.app_selected.id
