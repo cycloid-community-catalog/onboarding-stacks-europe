5adaf53ff273bbe58774a58e32df681edcf914b8
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-04-24 09:24:59 +0200

    minor fix

--- stack-compute-azure-demo/terraform/module-compute/network.tf
+++ stack-compute-azure-demo/terraform/module-compute/network.tf
@@ -37,7 +37,7 @@ resource "azurerm_network_security_rule" "inbound" {
   source_port_range          = "*"
   destination_address_prefix = "*"
   destination_port_range     = "${element(var.vm_ports_in, count.index)}"
-  protocol                   = "TCP"
+  protocol                   = "Tcp"
 }
 
 # Get a Static Public IP
