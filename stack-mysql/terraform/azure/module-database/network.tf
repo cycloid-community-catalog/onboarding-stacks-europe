0ec644edcede8e0998fb16ab0a6cabbb278f9210
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-30 15:20:07 +0200

    minor fix

--- stack-mysql/terraform/azure/module-database/network.tf
+++ stack-mysql/terraform/azure/module-database/network.tf
@@ -1,4 +1,4 @@
-resource "azurerm_postgresql_flexible_server_firewall_rule" "firewall_rule" {
+resource "azurerm_mysql_flexible_server_firewall_rule" "firewall_rule" {
   name             = "AllowAllAzureIPs"
   server_id        = azurerm_mysql_flexible_server.mysql.id
   start_ip_address = "0.0.0.0"
