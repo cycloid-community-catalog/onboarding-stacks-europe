9c408ead815d96bd50e0309f6b77fe966995af15
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-30 14:00:56 +0200

    minor fix

--- stack-mysql/terraform/aws/outputs.tf
+++ stack-mysql/terraform/aws/outputs.tf
@@ -2,13 +2,3 @@ output "rds_endpoint" {
   description = "The connection endpoint for the RDS instance"
   value       = module.rds.rds_endpoint
 }
-
-output "rds_port" {
-  description = "The port on which the DB accepts connections"
-  value       = module.rds.rds_port
-}
-
-output "database_name" {
-  description = "The name of the database"
-  value       = module.rds.database_name
-} 
