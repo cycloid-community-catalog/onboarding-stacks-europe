87864742979753edd37427520357178c1eb67595
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-30 13:50:41 +0200

    minor fix

--- stack-postgresql/terraform/gcp/module-database/user.tf
+++ stack-postgresql/terraform/gcp/module-database/user.tf
@@ -1,5 +1,5 @@
 resource "google_sql_user" "user" {
   name     = var.database_user
   instance = google_sql_database_instance.postgresql.name
-  password = var.database_password
+  password = random_password.db.result
 } 
